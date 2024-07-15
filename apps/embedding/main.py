3fb35190fc9d9621973873df85a20b1050867ade
nkumbe aurelien <nkaurelien@gmail.com>
2024-07-15 09:41:00 +0200

    Embedding plus + rag

---
+++ apps/embedding/main.py
@@ -0,0 +1,107 @@
+import pprint
+# Load docs
+# from langchain.document_loaders import WebBaseLoader
+# # loader = WebBaseLoader("https://www.smartdatapay.com/conventions/KALICONT000005635624.html")
+# loader = WebBaseLoader("https://travail-emploi.gouv.fr/droit-du-travail/les-absences-pour-maladie-et-conges-pour-evenements-familiaux/article/les-conges-pour-evenements-familiaux-et-le-conge-de-deuil")
+# docs = loader.load()
+
+# print(docs)
+
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
+loader = DirectoryLoader('docs', glob="**/*.md", show_progress=True, loader_cls=TextLoader)
+docs = loader.load()
+
+# print(docs[:5])
+
+
+# embedding
+
+from langchain_community.embeddings import OllamaEmbeddings
+
+EMBEDDING_MODEL="nomic-embed-text"
+OLLAMA_SERVER_URL="http://localhost:7869"
+# EMBEDDING_MODEL="mxbai-embed-large"
+
+embeddings = (
+    OllamaEmbeddings(model=EMBEDDING_MODEL, base_url = OLLAMA_SERVER_URL )
+)
+
+# query_result = embeddings.embed_query(str(docs[0]))
+
+# Split
+
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+
+headers_to_split_on = [
+    # ("#", "Header1"),
+    # ("##", "Header2"),
+    # ("###", "Header3"),
+    ("####", "Header4"),
+    # ("#####", "Header5"),
+]
+markdown_splitter = MarkdownHeaderTextSplitter(
+    headers_to_split_on=headers_to_split_on, strip_headers=False
+)
+md_header_splits = []
+for docu in docs:
+    md_header_splits.extend(markdown_splitter.split_text(docu.page_content))
+
+
+# from langchain_experimental.text_splitter import SemanticChunker
+# text_splitter = SemanticChunker(embeddings)
+# def format_docs(docs):
+#     return "\n\n".join(doc.page_content for doc in docs)
+# split_documents = text_splitter.create_documents([format_docs(docs)])
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=0,
+    length_function=len,
+    add_start_index=True,
+    keep_separator=True
+)
+split_documents = text_splitter.split_documents(docs)
+# split_documents = text_splitter.create_documents([doc.page_content for doc in docs])
+
+
+
+# print(md_header_splits[:5])
+# print(docs[:5])
+# # print(docs[-1:5])
+#
+# exit(0)
+#
+
+
+
+
+from langchain_milvus.vectorstores import Milvus
+
+# The easiest way is to use Milvus Lite where everything is stored in a local file.
+# If you have a Milvus server you can use the server URI such as "http://localhost:19530".
+# URI = "./milvus_demo.db"
+URI = "http://localhost:19530"
+
+
+vector_db = Milvus.from_documents(
+    md_header_splits,
+    embeddings,
+    connection_args={"uri": URI},
+    collection_name="syntheses_ccn",
+    drop_old=True,  # Drop the old Milvus collection if it exists
+)
+
+vector_db = Milvus.from_documents(
+    split_documents,
+    embeddings,
+    connection_args={"uri": URI},
+    collection_name="syntheses_ccn_splits",
+    drop_old=True,  # Drop the old Milvus collection if it exists
+)
+
+# query = "Les congés pour événements familiaux et le congé de deuil"
+# docs = vector_db.similarity_search(query)
+#
+# print(docs[:5])
---
+++ apps/embedding/query.py
@@ -0,0 +1,49 @@
+
+from .RAGHandler import RAGHandler
+# embedding
+
+from langchain_community.embeddings import OllamaEmbeddings
+import json
+from pymilvus import MilvusClient
+
+CLUSTER_ENDPOINT="http://localhost:19530"
+# 1. Set up a Milvus client
+client = MilvusClient(
+    uri=CLUSTER_ENDPOINT,
+    # token=TOKEN
+)
+
+EMBEDDING_MODEL="nomic-embed-text"
+OLLAMA_SERVER_URL="http://localhost:7869"
+
+embeddings = (
+    OllamaEmbeddings(model=EMBEDDING_MODEL, base_url = OLLAMA_SERVER_URL )
+)
+
+query = "Essai et préavis"
+
+query_embedding = embeddings.embed_query(query)
+
+print(query_embedding[:5])
+
+from langchain_milvus.vectorstores import Milvus
+
+# The easiest way is to use Milvus Lite where everything is stored in a local file.
+# If you have a Milvus server you can use the server URI such as "http://localhost:19530".
+# URI = "./milvus_demo.db"
+URI = "http://localhost:19530"
+
+
+# Single vector search
+res = client.search(
+    collection_name="syntheses_conventions_collectives",
+    # Replace with your query vector
+    # data=[[0.3580376395471989, -0.6023495712049978, 0.18414012509913835, -0.26286205330961354, 0.9029438446296592]],
+    data=[query_embedding],
+    limit=5, # Max. number of search results to return
+    # search_params={"metric_type": "IP", "params": {}} # Search parameters
+)
+
+# Convert the output to a formatted JSON string
+result = json.dumps(res, indent=4)
+print(result)
